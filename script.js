88dcf5772f76e562a39f1b0c2e191edd2cdc20b0
Prabhu-17 <prabhuponnusamy35703@gmail.com>
2025-06-10 09:12:01 +0530

    index

---
+++ script.js
@@ -0,0 +1,10 @@
+// function greet(name ,age){
+//     return `hello ${name} is ${age} old`
+// }
+// console.log(greet('Ronaldo',40));
+
+const choices = ['rock', 'paper', 'scissor']
+let computer = choices[Math.floor(Math.random() * 3)]
+console.log(computer);
+
+
